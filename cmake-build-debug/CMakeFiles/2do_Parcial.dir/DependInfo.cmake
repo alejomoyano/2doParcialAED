afcf32a41114c2e6af4526105bcaa1946c3a018f
Alejo Moyano <alejomoyano16@gmail.com>
2020-05-28 10:49:15 -0300

    primer commit

---
+++ cmake-build-debug/CMakeFiles/2do_Parcial.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/alejo/Documents/Facultad/AyED/2do Parcial/main.cpp" "C:/Users/alejo/Documents/Facultad/AyED/2do Parcial/cmake-build-debug/CMakeFiles/2do_Parcial.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/2do_Parcial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2do_Parcial.exe"
+  "2do_Parcial.exe.manifest"
+  "2do_Parcial.pdb"
+  "CMakeFiles/2do_Parcial.dir/main.cpp.obj"
+  "lib2do_Parcial.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2do_Parcial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
